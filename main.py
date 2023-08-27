89f3728cc870f8fee8d2f2a876dedd5177e12086
Minh Vu <ngocminhvu883@gmail.com>
2023-08-27 16:22:11 +0700

    Track data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import spotipy
 import json
+import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 
 try:
@@ -10,13 +11,25 @@ except FileNotFoundError:
 except json.JSONDecodeError:
     print('Error decoding JSON in the config file.')
 
-# Authentication without user
+columns = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness',
+           'liveness', 'valence', 'tempo', 'type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+
+track_data = pd.DataFrame(columns=columns)
+
+# authentication without user
 client_credentials_manager = SpotifyClientCredentials(
     client_id=config_data['client_id'], client_secret=config_data['client_secret'])
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 playlist_link = 'https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f'
 playlist_URI = playlist_link.split('/')[-1].split('?')[0]
-track_uris = [x['track']['uri']
-              for x in sp.playlist_tracks(playlist_URI)['items']]
-print(track_uris)
+
+for track in sp.playlist_tracks(playlist_URI)['items']:
+    track_uri = track['track']['uri']
+
+    track_features = pd.DataFrame(
+        [sp.audio_features(track_uri)[0]], columns=columns)
+
+    track_data = pd.concat([track_data, track_features], ignore_index=True)
+
+print(track_data.head())
